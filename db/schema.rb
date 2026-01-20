832b9fe73d9314f28d3e1abbbe4e110df9295975
Vojtech Rinik <vojto@rinik.net>
2026-01-20 10:59:45 +0100

    Update schema for transaction invoice link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_201901) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_20_090000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -117,6 +117,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_201901) do
     t.text "description"
     t.datetime "hidden_at"
     t.string "internal_transaction_id"
+    t.bigint "invoice_id"
     t.boolean "is_enriched", default: false, null: false
     t.integer "original_amount_cents"
     t.string "original_currency"
@@ -127,6 +128,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_201901) do
     t.index ["bank_connection_id", "internal_transaction_id"], name: "idx_on_bank_connection_id_internal_transaction_id_7e9d222c1d", unique: true
     t.index ["bank_connection_id"], name: "index_transactions_on_bank_connection_id"
     t.index ["booking_date"], name: "index_transactions_on_booking_date"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -157,4 +159,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_201901) do
   add_foreign_key "invoices", "emails"
   add_foreign_key "invoices", "users"
   add_foreign_key "transactions", "bank_connections"
+  add_foreign_key "transactions", "invoices"
 end
